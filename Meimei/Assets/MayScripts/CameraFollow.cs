8a637b0429116094ad153ad7e6d9598714829de8
Lilinamay <82954244+Lilinamay@users.noreply.github.com>
2022-11-19 18:43:49 -0500

    walk scene

---
+++ Meimei/Assets/MayScripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public Vector3 cameraOffset=new Vector3(-5,-3);
+    public float cameraSpeed = 0.1f;
+    void Start()
+    {
+        //.position = player.position + cameraOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        if (PlayerWalk.camTrigger)
+        {
+            Vector3 finalPosition = player.position + cameraOffset;
+            Vector3 lerpPosition = Vector3.Lerp(transform.position, finalPosition, cameraSpeed);
+            transform.position = new Vector3(lerpPosition.x, transform.position.y, -10);
+        }
+    }
+}
---
+++ Meimei/Assets/MayScripts/PlayerWalk.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerWalk : MonoBehaviour
+{
+    private Touch t;
+    float speed = 0;
+    bool down = false;
+    public static bool camTrigger = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position -= new Vector3(speed * Time.deltaTime, 0);
+        if (Input.touchCount > 0)
+        {
+            t = Input.GetTouch(0);
+            if (t.phase == TouchPhase.Began)
+            {
+                if (Camera.main.ScreenToWorldPoint(t.position).x <= this.transform.position.x)
+                {
+                    //start animation
+                    speed = 2;
+                }
+            }
+            else if (t.phase == TouchPhase.Ended)
+            {
+                down = true;
+                DOTween.To(() => speed, x => speed = x, 0, 1).SetEase(Ease.InQuart).OnComplete(() =>
+                {
+                    //    a = 5;
+                    //});
+                    Debug.Log("finished");
+                });
+            }
+        }
+
+        //if (down)
+        //{
+        //    if (speed > 0)
+        //    {
+        //        speed -= Time.deltaTime;
+        //    }
+        //    else
+        //    {
+        //        down = false;
+        //    }
+        //}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name== "camTrigger")
+        {
+            Debug.Log("triggered");
+            camTrigger = true;
+        }
+    }
+}
